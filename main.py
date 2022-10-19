fc7fd7fbcd1d0b66e8a8dc4d07eaa7c0f4950efa
Felipe Ferraz <111993117+FelipeFerraz98@users.noreply.github.com>
2022-10-19 20:03:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+precision = 1e-07
+
+k = 0
+x = int(input("Enter a value you want to calculate the root of: "))
+e = x
+i = int(input("Enter an index for the root: "))
+
+fx = x**i - e
+
+#Newton's Algorithm for Root Calculation // Algoritmo de Newton para calculo das raizes
+while abs(fx) > precision:
+
+    derivativeFX = i*pow(x, i-1)
+    x = x - (fx/derivativeFX)
+    fx = pow(x, i) - e
+
+    k += 1 #Attempts count // contador de tentativas
+
+    print("\n{}* Result = {:.8f}\n".format(k, x))
+
+
+print("Final result in {} attempts = {:.8f}".format(k,x))
